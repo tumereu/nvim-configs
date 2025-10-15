dec5e2a9343d24d073e54849e2e130264b3c7b53
Tuomas Vaulanen <tuomas.vaulanen@optiscangroup.com>
2025-10-15 08:28:12 +0300

    Fixed some vim surround/leap issues

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,4 @@ vim.api.nvim_set_keymap("i", "<C-BS>", "<Esc>caw", { noremap = true })
 vim.api.nvim_set_keymap("i", "<C-H>", "<Esc>caw", { noremap = true })
 
 -- Double-space to save all in normal model
-vim.api.nvim_set_keymap("n", "<space><space>", ":wall<Enter>", { noremap = true })
-
--- Ctrl-Enter to perform code actions
-vim.api.nvim_set_keymap("n", "<Ctrl-Enter>", "<leader>ca", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader><leader>", ":wall<Enter>", { noremap = true })
--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -3,8 +3,26 @@ return {
     "kylechui/nvim-surround",
     version = "^3.0.0",
     event = "VeryLazy",
+    init = function()
+      -- Remove visual mode x so that it can be used to add surroundings via nvim.surround
+      vim.keymap.del("v", "x")
+    end,
     config = function()
-      require("nvim-surround").setup({})
+      require("nvim-surround").setup({
+        keymaps = {
+          insert = "<c-g>x",
+          insert_line = "<c-g>X",
+          normal = "yx",
+          normal_cur = "yxx",
+          normal_line = "yX",
+          normal_cur_line = "yXX",
+          visual = "x",
+          visual_line = "X",
+          delete = "dx",
+          change = "cx",
+          change_line = "cX",
+        },
+      })
     end,
   },
 }
